9eb2e22e7275ab305d558fe72d9bf9a90564066c
manu <manu@manu-All-Series>
2019-04-24 18:30:13 +0200

    esd

--- RosendoPinedaManuelÁngel/src/Ejercicio1.java
+++ RosendoPinedaManuelÁngel/src/Ejercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//Cambio del alumno 1 (Manuel Ángel Rosendo Pineda 1) perita
+//Cambio del alumno 1 (Manuel Ángel Rosendo Pineda 1) peritads
 
 public class Ejercicio1 {
 		
